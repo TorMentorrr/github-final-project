c32c9ce1ae4f066d7214f16a53e81719a1e33639
TorMentorrr <114949129+TorMentorrr@users.noreply.github.com>
2023-10-11 11:15:21 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <TorMentorrr>
    # Input:
    # p, principal amount
    # t, time period in years
